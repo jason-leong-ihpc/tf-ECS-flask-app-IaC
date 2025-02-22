febd84a57acf41dbdb52b4caf952c61d43bdab68
jason-leong-ihpc <jason-leong@ihpc.a-star.edu.sg>
2025-02-22 11:00:00 +0800

    add s3

--- main.tf
+++ main.tf
@@ -2,4 +2,18 @@
 #   name_prefix = split("/", data.aws_caller_identity.current.arn)[1]
 # }
 
+resource "aws_s3_bucket" "static_bucket" {
+  bucket        = "${var.s3_bucket_name}"
+  force_destroy = true
+}
+
+
+resource "aws_s3_bucket_public_access_block" "enable_public_access" {
+  bucket = aws_s3_bucket.static_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = false
+  ignore_public_acls      = true
+  restrict_public_buckets = false
+}
 
--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "aws_region" {
   type        = string
   default     = "ap-southeast-1"
 }
+
+variable "s3_bucket_name" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "ce8-coaching18-s3"
+}
