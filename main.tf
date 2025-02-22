9d532308e4a8ab3e2847a78dfdc99ec53c36ff1c
jason-leong-ihpc <jason-leong@ihpc.a-star.edu.sg>
2025-02-22 11:18:30 +0800

    add ecr, ecs

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
-# locals {
-#   name_prefix = split("/", data.aws_caller_identity.current.arn)[1]
-# }
+locals {
+  name_prefix = "ce8-coaching18"
+}
 
 resource "aws_s3_bucket" "static_bucket" {
-  bucket        = var.s3_bucket_name
+  bucket        = "${local.name_prefix}-s3"
   force_destroy = true
 }
 
 
-resource "aws_s3_bucket_public_access_block" "enable_public_access" {
-  bucket = aws_s3_bucket.static_bucket.id
+# resource "aws_s3_bucket_public_access_block" "enable_public_access" {
+#   bucket = aws_s3_bucket.static_bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = false
-  ignore_public_acls      = true
-  restrict_public_buckets = false
-}
+#   block_public_acls       = true
+#   block_public_policy     = false
+#   ignore_public_acls      = true
+#   restrict_public_buckets = false
+# }
 
